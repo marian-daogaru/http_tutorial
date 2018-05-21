2668473cefda517d25065243ad0cd90d488cb53f
marian-daogaru <marian@phyramid.com>
2018-05-21 17:20:56 +0300

    l152 + git ignore

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -14,8 +14,15 @@ class Blog extends Component {
   componentDidMount () {
     axios.get('https://jsonplaceholder.typicode.com/posts')
       .then(response => {
+        const posts = response.data.slice(0, 4)
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: 'MD'
+          }
+        })
         this.setState({
-          posts: response.data
+          posts: updatedPosts
         })
         console.log(response)
       })
@@ -26,6 +33,7 @@ class Blog extends Component {
       return <Post
         key={post.id}
         title={post.title}
+        author={post.author}
       />
     })
 
