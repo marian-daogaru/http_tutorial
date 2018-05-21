02252ed02d722ff2552d97a6f98887f444b73e75
marian-daogaru <marian@phyramid.com>
2018-05-21 17:26:54 +0300

    l153

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import './Blog.css';
 
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [],
+    selectedPostId: null,
   }
 
   componentDidMount () {
@@ -28,12 +29,17 @@ class Blog extends Component {
       })
   }
 
+  postSelectedHandler = (id) => {
+    this.setState({selectedPostId: id})
+  }
+
   render () {
     const posts = this.state.posts.map((post, index) => {
       return <Post
         key={post.id}
         title={post.title}
         author={post.author}
+        clicked={() => {this.postSelectedHandler(post.id)}}
       />
     })
 
@@ -43,7 +49,9 @@ class Blog extends Component {
           {posts}
         </section>
         <section>
-          <FullPost />
+          <FullPost
+            id ={this.state.selectedPostId}
+          />
         </section>
         <section>
           <NewPost />
