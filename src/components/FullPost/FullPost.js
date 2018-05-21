85789a5b7999b2023ea2b9ba05652adb6ffe514d
marian-daogaru <marian@phyramid.com>
2018-05-21 17:46:56 +0300

    l156

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -28,6 +28,13 @@ class FullPost extends Component {
     }
   }
 
+  deletePostHandler = () => {
+    axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+      .then(response => {
+        console.log(response)
+      })
+  }
+
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
         if (this.state.id) {
@@ -39,7 +46,10 @@ class FullPost extends Component {
                   <h1>{this.state.loadedPost.title}</h1>
                   <p>{this.state.loadedPost.body}</p>
                   <div className="Edit">
-                      <button className="Delete">Delete</button>
+                      <button
+                        className="Delete"
+                        onClick={this.deletePostHandler}
+                      >Delete</button>
                   </div>
               </div>
           )
