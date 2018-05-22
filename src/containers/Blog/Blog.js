10b246a3ecd25f6fa1c4f1a00fa2da5329be53e6
marian-daogaru <marian@phyramid.com>
2018-05-22 12:56:58 +0300

    l183

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import {Route} from 'react-router-dom'
+import {Route, Link} from 'react-router-dom'
 
 // import axios from 'axios'
 // import axios from '../../axios'
 import Posts from './Posts/Posts'
-
+import NewPost from './NewPost/NewPost'
 import './Blog.css';
 
 class Blog extends Component {
@@ -14,14 +14,23 @@ class Blog extends Component {
         <header>
           <nav>
             <ul>
-              <li><a href='/'>Home</a></li>
-              <li><a href='/new-post'>New Post</a></li>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to={{
+                pathname: '/new-post',
+                hash: '#submit',
+                search: '?quick-submit=true',
+            }}>New Post</Link></li>
             </ul>
           </nav>
         </header>
         <Route
           path='/'
           component={Posts}
+          exact
+        />
+        <Route
+          path='/new-post'
+          component={NewPost}
           exact
         />
       </div>
