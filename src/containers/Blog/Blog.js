24d61c577949d24789d73def743022861d3d97d5
marian-daogaru <marian@phyramid.com>
2018-05-22 14:30:05 +0300

    l189

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import {Route, Link} from 'react-router-dom'
+import {Route, NavLink} from 'react-router-dom'
 
 // import axios from 'axios'
 // import axios from '../../axios'
 import Posts from './Posts/Posts'
 import NewPost from './NewPost/NewPost'
+import FullPost from './FullPost/FullPost'
 import './Blog.css';
 
 class Blog extends Component {
@@ -14,12 +15,20 @@ class Blog extends Component {
         <header>
           <nav>
             <ul>
-              <li><Link to='/'>Home</Link></li>
-              <li><Link to={{
+              <li><NavLink
+                exact
+                to='/'
+                activeClassName='my-active'
+                activeStyle={{
+                  color: '#fa923f',
+                  textDecoration: 'underline',
+                }}
+              >Home</NavLink></li>
+              <li><NavLink to={{
                 pathname: '/new-post',
                 hash: '#submit',
                 search: '?quick-submit=true',
-            }}>New Post</Link></li>
+            }}>New Post</NavLink></li>
             </ul>
           </nav>
         </header>
@@ -31,6 +40,11 @@ class Blog extends Component {
         <Route
           path='/new-post'
           component={NewPost}
+          exact
+        />
+        <Route
+          path='/:id'
+          component={FullPost}
           exact
         />
       </div>
